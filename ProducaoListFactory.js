da2c801897e4514c14e0c97b20fd1c77883a130b
Kauê Farias Lourenço <162914687+fariasss29@users.noreply.github.com>
2025-09-18 12:02:41 -0300

    Create ProducaoListFactory.js

---
+++ ProducaoListFactory.js
@@ -0,0 +1,20 @@
+import ProducaoListController from "@/controllers/bps/producao/ProducaoListController";
+import GetAreasProducao from "@/useCases/bps/producao/GET/GetAreasProducao";
+import GetDemandaArea from "@/useCases/bps/producao/GET/GetDemandaArea";
+import GetL2LStatus from "@/useCases/bps/producao/GET/GetL2LStatus";
+import GetMD47 from "@/useCases/bps/producao/GET/GetMD47";
+import { GetSQCDAnual } from "@/useCases/bps/producao/GET/GetSqdcAnual";
+import { GetHse } from "@/useCases/bps/producao/GET/GetHse";
+import { minioClient }    from "@/services/minioCliente";
+
+
+export default function ProducaoListFactory(conn) {
+    return new ProducaoListController({
+        consultarHoraHora: new GetL2LStatus(conn),
+        consultarMD47: new GetMD47(conn),
+        consultarDemandasArea: new GetDemandaArea(conn),
+        consultarAreasProducao: new GetAreasProducao(conn),
+        consultarSQDCAnual: new GetSQCDAnual(conn),
+        consultarHSE: new GetHse({ db: conn, minio: minioClient }),
+    })
+}
